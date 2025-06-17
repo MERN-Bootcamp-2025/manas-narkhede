92adb1681ffdef65f4be7a00629a3dee8d61bc55
manas-narkhede <manas.narakhede@mindbowser.com>
2025-06-17 18:51:25 +0530

    committing changes

---
+++ week2/Node/Day5/Event-loop-demo/event-loop-demo.js
@@ -0,0 +1,50 @@
+
+
+//process.nextTick() - MicroStack queue
+
+console.log('Start')
+
+process.nextTick(()=>{
+    console.log('Inside process nextTick')
+})
+
+console.log('End')
+
+//Process.nextTick() runs after the current operation but before any I/O or timers
+//Even though its asynchrnous it executes before setTimeout and setImmediate
+
+
+
+//setTimeout() - Timer resolution
+
+console.log('Starting setTimeout')
+
+setTimeout(()=>{
+    console.log('Inside setTimeout')
+})
+
+console.log('Ending setTimout')
+
+//setTimeout(..., 0) executes in the Timers Phase of the event loop.
+//It's scheduled after the current call stack is empty, even if the delay is 0.
+
+
+//setImmediate 
+
+console.log('Starting setImmediate')
+
+setImmediate(()=>{
+    console.log('Starting with setImmediate')
+})
+
+console.log('Ending setImmediate ')
+
+//setImmediate() callbacks are invoked here
+
+
+
+
+
+
+
+
